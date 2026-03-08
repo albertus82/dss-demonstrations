d27aa2136f0c7924531640af17530a7136db08ba
Alberto <albertus82@users.noreply.github.com>
2026-03-08 22:17:33 +0100

    Memory limit

--- dss-app.sh
+++ dss-app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LD_LIBRARY_PATH="/usr/lib/java/javafx-25/lib"
-"/usr/lib/jvm/java-1.25.0-openjdk-arm64/bin/java" -Xms32m --module-path $LD_LIBRARY_PATH --add-modules=javafx.controls,javafx.fxml --enable-native-access=javafx.graphics -jar "dss-standalone-app-package/target/dss-app.jar"
+"/usr/lib/jvm/java-1.25.0-openjdk-arm64/bin/java" -Xms16m -Xmx256m --module-path $LD_LIBRARY_PATH --add-modules=javafx.controls,javafx.fxml --enable-native-access=javafx.graphics -jar "dss-standalone-app-package/target/dss-app.jar"
